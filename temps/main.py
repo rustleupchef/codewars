dd7922dbb4253501fe16a3d91cba94b7d8bb8d6e
rustleupchef <rustle.up.chef@gmail.com>
2025-02-01 17:56:19 -0600

    added file to automatically create empty solutions

---
+++ temps/main.py
@@ -0,0 +1,20 @@
+import os
+
+def main():
+    problems: int
+    while True:
+        try:
+            problems = int(input("Number of problems:"))
+            break
+        except ValueError:
+            print("Please enter valid number of problems")
+    problems += 1
+    length:int = len(str(problems))
+    for i in range(problems):
+        zeros = "0" * (length - len(str(i)))
+        if not os.path.exists(f"{zeros}{i}.py"):
+            with open(f"{zeros}{i}.py", 'x') as file:
+                file.close();
+    
+if __name__ == '__main__':
+    main()
