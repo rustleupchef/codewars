232c15f0971a9d5e77944f97d5c1be5f5badd1a0
goober <123332908+rustleupchef@users.noreply.github.com>
2025-02-26 11:38:36 -0600

    Update 15.py

--- 2023/15.py
+++ 2023/15.py
@@ -1,8 +1,9 @@
 nums = [int(num) for num in input().split(',')]
 dices, faces = nums[0], nums[1]
 nums = [int(num) for num in input().split(',')]
-for num in nums:
-    num += -2 if num % 2 == 0 else 3
+for i  in range(dices):
+    num = nums[i]
+    num += -2 if i % 2 == 0 else 3
     num %= faces
     if num == 0: num = faces
     print(num, end=" ")
