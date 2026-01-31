795cb0e15a399f8a381533dfbd91c3d77eb95f8b
rustleupchef <rustle.up.chef@gmail.com>
2026-01-31 14:07:44 -0600

    added solution 4

--- 2023leon/problem_4/main.py
+++ 2023leon/problem_4/main.py
@@ -1,6 +1,12 @@
 #Problem 4 solution
+import re
 def main():
-    pass
-
-if __name__ == "__main__":
+    match = re.match(r'(?P<number>\d+)(?P<string>\w+)', input())
+    if match is None:
+        print('Error: invalid input')
+        return
+    times = int(match.group('number'))
+    string = match.group('string')
+    print(string*times)
+if __name__ == '__main__':
     main()
