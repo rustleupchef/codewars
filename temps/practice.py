dc939937363434c511d114d4ebcb74c685180ae8
rustleupchef <rustle.up.chef@gmail.com>
2026-01-31 12:02:59 -0600

    added practice creation tool; added merge tool; added templates

---
+++ temps/practice.py
@@ -0,0 +1,49 @@
+import sys
+import os
+
+def main(arguments:list[str] = []):
+    count = 0
+    working_dir = ""
+
+    count = int(arguments[0]) if len(arguments) > 0 else count
+    working_dir = arguments[1] if len(arguments) > 1 else working_dir
+
+    os.makedirs(working_dir, exist_ok=True)
+
+    print(f"Designing file for {count} problem(s)")
+
+    with open("temps/templates/main.py", "r") as f:
+        template = f.read()
+        f.close()
+
+    with open("temps/templates/description.md", "r") as f:
+        description = f.read()
+        f.close()
+    
+    with open ("temps/templates/merge.py", "r") as f:
+        merge_script = f.read()
+        f.close()
+
+    for i in range(count):
+        print("-" * 20)
+        print(f"Working on problem {i + 1}")
+
+        os.path.exists(working_dir) or os.makedirs(working_dir)
+        os.makedirs(f"{working_dir}/problem_{i + 1}/", exist_ok=True)
+        with open(f"{working_dir}/problem_{i + 1}/input.txt", "w") as f:
+            f.close()
+
+        with open(f"{working_dir}/problem_{i + 1}/main.py", "w") as f:
+            f.write(f"#Problem {i +1} solution\n{template}")
+            f.close()
+
+        with open(f"{working_dir}/problem_{i + 1}/description.md", "w") as f:
+            f.write(description)
+            f.close()
+
+    with open(f"{working_dir}/merge.py", "w") as f:
+        f.write(merge_script)
+        f.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
