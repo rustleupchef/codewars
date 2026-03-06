83ce9a83c4934967cef38a9a8ffefd57f98e973e
rustleupchef <rustle.up.chef@gmail.com>
2026-03-06 08:32:39 -0600

    added problem_26 solution for 2023 practice

--- 2023practice/problem_26/main.py
+++ 2023practice/problem_26/main.py
@@ -1,6 +1,37 @@
 #Problem 26 solution
+def grabFactors(num: int, needs=tuple()):
+    factors = []
+    for i in range(1, num + 1):
+        if i == 1 or i == num:
+            continue
+        if num % i == 0:
+            factors.append(sorted(needs + (i, num // i)))
+            seed1 = grabFactors(i, needs + (num // i, ))
+            if seed1:
+                factors.extend(sorted(seed1))
+    return tuple(factors)
+
 def main():
-    pass
+    nums = []
+    while True:
+        num = int(input())
+        if num == 0:
+            break
+        nums.append(num)
+
+    for num in nums:
+        factors = grabFactors(num)
+        filters = []
+
+        for factor in factors:
+            if factor[::-1] not in filters:
+                filters.append(factor[::-1])
+        filters = sorted(filters, key=lambda x: x[0])[::-1]
+        if filters == []:
+            continue
+        filters = [[str(x) for x in combo] for combo in filters]
+        combinations = [f'({"*".join(combo)})' for combo in filters]
+        print(f"{num}={','.join(combinations)}")
 
 if __name__ == "__main__":
     main()
